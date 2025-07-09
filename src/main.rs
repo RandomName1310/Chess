53b0a11ad5599847a74d9d6d8fa3e2792b8ad9ed
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2025-07-09 15:57:48 -0300

    Fixed all errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod moves;
 use moves::*;
 
+mod board;
+use board::*;
+
+use macroquad::prelude::*;
+
 const BOARD_SIZE: usize = 8;
+const X_OFFSET: f32 = 100.0;
+const Y_OFFSET: f32 = 100.0;
+
+type Board = [[&'static str; 8]; 8];
 
 #[derive(Copy, Clone)]
 enum PieceType {
@@ -23,72 +32,32 @@ enum Color {
 struct Piece {
     piece_type: PieceType,
     color: Color,
-    moves_move: &'static[Offset],
-    moves_capture: &'static[Offset]
+    moves_move: &'static [Offset],
+    moves_capture: &'static [Offset],
 }
 
-type Board = [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE]; // type makes something equivalent
-// example: Board now is equal to [[Option<Piece>; BOARD_SIZE]; BOARD_SIZE]. Makes it simpler
-
-
-// set pieces
-fn init_board() -> Board{
-    let mut board: Board = [[None; BOARD_SIZE]; BOARD_SIZE];
-
-    // place pawns
-    for x in 0..BOARD_SIZE{
-        board[1][x] = Some(Piece { piece_type: PieceType::Pawn, color: Color::Black });
-        board[6][x] = Some(Piece { piece_type: PieceType::Pawn, color: Color::White });
-    }
-
-    // place rooks
-    board[7][7] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
-    board[7][0] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
-    board[0][0] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
-    board[0][7] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
-
-    // place knight
-    board[7][6] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
-    board[7][1] = Some(Piece { piece_type: PieceType::Rook, color: Color::White });
-    board[0][6] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
-    board[0][1] = Some(Piece { piece_type: PieceType::Rook, color: Color::Black });
-
-    board
-}
-
-// create board tiles
-fn create_board(board: &Board){
-    const X_OFFSET: f32 = 100.0;
-    const Y_OFFSET: f32 = 100.0;
-    let is_white: bool = true;
-
-    for x in 0..BOARD_SIZE{
-        for y in 0..BOARD_SIZE{
-            let piece: Option<Piece> = board[y][x];
-            let letter: if let Some(piece) = piece {
-                match piece.piece_type{
-                    PieceType::Pawn => letter = String::from("P");
-                    PieceType::Rook => letter = String::from("R");
-                    PieceType::Knight => letter = String::from("N");
-                    PieceType::Bishop => letter = String::from("B");
-                    PieceType::King => letter = String::from("K");
-                    PieceType::Queen => letter = String::from("Q");
-                    _ => letter = String::from("ERR");
-                }
-            }
-
-            draw_text(&format!("{}", letter), 20.0 + X_OFFSET * x, Y_OFFSET * y, 20.0, RED);
+// draw the board with the pieces on screen
+fn draw_board(board: &mut Board) {
+    for x in 0..BOARD_SIZE {
+        for y in 0..BOARD_SIZE {
+            let piece: &str = board[y][x];
+            draw_text(&format!("{}", piece), 20.0 + X_OFFSET * x as f32, 20.0 + Y_OFFSET * y as f32, 20.0, RED);
         }
     }
 }
 
-fn move_piece(board: &mut Board, coord: (i8, i8)){
-
+fn move_piece(_board: &mut Board, _coord: (i8, i8)) {
+    // later...
 }
 
-#[macroquad::main("Jogo 2D Simples")]
-async fn main() {
-    let mut board: Board = init_board();
-    create_board(&board);
-    move_piece(&mut board, (1, 2));
+#[macroquad::main("Jogo de Xadrez")]
+async fn main(){
+    let mut board: Board = create_board();
+
+    loop {
+        clear_background(WHITE);
+        draw_board(&mut board);
+        move_piece(&mut board, (1, 2));
+        next_frame().await;
+    }
 }
