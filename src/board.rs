6fd7da1e91852d17cc219311062deaeb0261d6db
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2025-07-11 16:58:23 -0300

    square selection

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,19 @@
-pub fn create_board() ->  [[&'static str; 8]; 8]{
-    let mut board_structure: [[&str; 8]; 8] = [
+use crate::Board;
+
+pub const BOARD_LAYOUT: [[&'static str; 8]; 8] = 
+    [
+        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 0 (top)
+        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 1
+        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 2
+        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 3
+        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 4
+        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 5
+        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 6
+        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 7 (bottom)
+    ];
+
+pub const PIECE_LAYOUT: [[&'static str; 8]; 8] =
+    [
         // black
         ["R", "N", "B", "Q", "K", "B", "N", "R"],
         ["P", "P", "P", "P", "P", "P", "P", "P"],
@@ -13,5 +27,8 @@ pub fn create_board() ->  [[&'static str; 8]; 8]{
         ["p", "p", "p", "p", "p", "p", "p", "p"],
         ["r", "n", "b", "q", "k", "b", "n", "r"],
     ];
-    board_structure
+
+// makes all squares return to their original colors
+pub fn refresh_board(board: &mut Board){
+    *board = BOARD_LAYOUT;
 }
--- src/main.rs
+++ src/main.rs
@@ -35,44 +35,63 @@ struct Piece {
     moves_capture: &'static [Offset],
 }
 
-// draw the board with the pieces on screen
-fn draw_board(board: &mut Board){
-    let mut is_white: bool = false;
-
+fn get_board_offset() -> (f32, f32){
     // calculate position based on screen size
     let board_px = BOARD_SIZE as f32 * SQUARE_SIZE;
     let x_offset = (screen_width() - board_px) / 2.0;
     let y_offset = (screen_height() - board_px) / 2.0;
 
+    (x_offset, y_offset)
+}
+
+fn get_selected_square() -> (usize, usize){
+    let mouse_pos: (f32, f32) = mouse_position();
+    let board_offset: (f32, f32) = get_board_offset();
+
+    // calculate specific square coord
+    let square_selected: (usize, usize) = (((mouse_pos.0 - board_offset.0) / SQUARE_SIZE) as usize, 
+                                           ((mouse_pos.1 - board_offset.1) / SQUARE_SIZE) as usize);
+
+    square_selected
+}
+
+// draw the board with the pieces on screen
+fn draw_board(board: &mut Board, pieces: &mut Board){
+    let board_offset: (f32, f32) = get_board_offset();
+
+    // set pieces and squares
     for x in 0..BOARD_SIZE {
-        is_white = !is_white;
         for y in 0..BOARD_SIZE {
-            let piece: &str = board[y][x];
+            let piece: &str = pieces[y][x];
 
-            let pos_x = x_offset + x as f32 * SQUARE_SIZE;
-            let pos_y = y_offset + y as f32 * SQUARE_SIZE;
+            let pos_x = board_offset.0 + x as f32 * SQUARE_SIZE;
+            let pos_y = board_offset.1 + y as f32 * SQUARE_SIZE;
 
-            if is_white{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
-            else{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)} 
+            if board[y][x] == "W"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
+            else if board[y][x] == "B"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)}
+            else if board[y][x] == "R"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, RED)} 
             draw_text(&format!("{}", piece), pos_x + 30.0, pos_y + 60.0, 50.0, RED);
-
-            is_white = !is_white;
         }
     }
 }
 
-fn move_piece(_board: &mut Board, _coord: (i8, i8)) {
-    // later...
+fn move_piece(board: &mut Board, _pieces: &mut Board, _coord: (i8, i8)) {
+    refresh_board(board);
+    let (square_x,square_y): (usize, usize) = get_selected_square();
+    board[square_y][square_x] = "R";
 }
 
 #[macroquad::main("Jogo de Xadrez")]
 async fn main(){
-    let mut board: Board = create_board();
+    let mut board: Board = BOARD_LAYOUT;
+    let mut pieces: Board = PIECE_LAYOUT;
 
     loop {
         clear_background(GRAY);
-        draw_board(&mut board);
-        move_piece(&mut board, (1, 2));
+        if is_mouse_button_pressed(MouseButton::Left) {
+            move_piece(&mut board, &mut pieces, (1, 2));
+        }
+        draw_board(&mut board, &mut pieces);
         next_frame().await;
     }
 }
