61cfcc74b9b64bdfe98afb1602675a5dccced7dd
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2025-07-11 18:58:05 -0300

    Primitive movement

--- src/board.rs
+++ src/board.rs
@@ -1,34 +1,32 @@
 use crate::Board;
 
-pub const BOARD_LAYOUT: [[&'static str; 8]; 8] = 
-    [
-        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 0 (top)
-        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 1
-        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 2
-        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 3
-        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 4
-        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 5
-        ["B", "W", "B", "W", "B", "W", "B", "W"], // row 6
-        ["W", "B", "W", "B", "W", "B", "W", "B"], // row 7 (bottom)
-    ];
+pub const BOARD_LAYOUT: [[char; 8]; 8] = [
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 0 (top)
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 1
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 2
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 3
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 4
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 5
+    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 6
+    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 7 (bottom)
+];
 
-pub const PIECE_LAYOUT: [[&'static str; 8]; 8] =
-    [
-        // black
-        ["R", "N", "B", "Q", "K", "B", "N", "R"],
-        ["P", "P", "P", "P", "P", "P", "P", "P"],
+pub const PIECE_LAYOUT: [[char; 8]; 8] = [
+    // black pieces
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
 
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
 
-        // white
-        ["p", "p", "p", "p", "p", "p", "p", "p"],
-        ["r", "n", "b", "q", "k", "b", "n", "r"],
-    ];
+    // white pieces
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
+];
 
-// makes all squares return to their original colors
+// makes all squares return to their original coll
 pub fn refresh_board(board: &mut Board){
     *board = BOARD_LAYOUT;
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use macroquad::prelude::*;
 const BOARD_SIZE: usize = 8;
 const SQUARE_SIZE: f32 = 80.0;
 
-type Board = [[&'static str; 8]; 8];
+type Board = [[char; 8]; 8];
 
 #[derive(Copy, Clone)]
 enum PieceType {
@@ -19,9 +19,10 @@ enum PieceType {
     Bishop,
     Queen,
     King,
+    Empty,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 enum Color {
     White,
     Black,
@@ -62,23 +63,62 @@ fn draw_board(board: &mut Board, pieces: &mut Board){
     // set pieces and squares
     for x in 0..BOARD_SIZE {
         for y in 0..BOARD_SIZE {
-            let piece: &str = pieces[y][x];
+            let piece: char = pieces[y][x];
 
             let pos_x = board_offset.0 + x as f32 * SQUARE_SIZE;
             let pos_y = board_offset.1 + y as f32 * SQUARE_SIZE;
 
-            if board[y][x] == "W"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
-            else if board[y][x] == "B"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)}
-            else if board[y][x] == "R"{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, RED)} 
-            draw_text(&format!("{}", piece), pos_x + 30.0, pos_y + 60.0, 50.0, RED);
+            if board[y][x] == 'W'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
+            else if board[y][x] == 'B'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)}
+            else if board[y][x] == 'R'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, RED)} 
+            draw_text(&format!("{}", piece), pos_x + 30.0, pos_y + 60.0, 50.0, GRAY);
         }
     }
 }
 
-fn move_piece(board: &mut Board, _pieces: &mut Board, _coord: (i8, i8)) {
-    refresh_board(board);
+// select a piece from the board
+fn select_piece(board: &mut Board, pieces: &mut Board) -> (usize, usize, PieceType, Color){
     let (square_x,square_y): (usize, usize) = get_selected_square();
-    board[square_y][square_x] = "R";
+    let piece: char = pieces[square_y][square_x];
+
+    let color: Color = if piece.is_uppercase() { Color::Black } else { Color::White };
+    let piece_type = match piece.to_ascii_lowercase(){
+        'p' => PieceType::Pawn,
+        'r' => PieceType::Rook,
+        'b' => PieceType::Bishop,
+        'n' => PieceType::Knight,
+        'q' => PieceType::Queen,
+        'k' => PieceType::King,
+        ' ' => PieceType::Empty,
+        _   => panic!("unknown symbol {}", piece),
+    };
+
+    // remove red squares from previous calls
+    refresh_board(board);
+
+    // create red square
+    board[square_y][square_x] = 'R';
+    (square_x,square_y, piece_type, color)
+}
+
+// move a piece from the board
+fn move_piece(board: &mut Board, pieces: &mut Board, last_piece_data: (usize, usize, PieceType, Color)){
+    let (square_x,square_y): (usize, usize) = get_selected_square();
+    let (piece_x, piece_y, piece_type, color): (usize, usize, PieceType, Color) = last_piece_data;
+
+    // remove red squares from previous calls
+    refresh_board(board);
+
+    pieces[piece_y][piece_x] = ' ';
+    pieces[square_y][square_x] = match piece_type{
+        PieceType::Pawn =>  if color == Color::Black{'P'}else{'p'},
+        PieceType::Bishop =>  if color == Color::Black{'B'}else{'b'},
+        PieceType::Knight =>  if color == Color::Black{'N'}else{'n'},
+        PieceType::Rook =>  if color == Color::Black{'R'}else{'r'},
+        PieceType::Queen =>  if color == Color::Black{'Q'}else{'q'},
+        PieceType::King =>  if color == Color::Black{'K'}else{'k'},
+        PieceType::Empty =>  ' ',
+    }
 }
 
 #[macroquad::main("Jogo de Xadrez")]
@@ -86,10 +126,18 @@ async fn main(){
     let mut board: Board = BOARD_LAYOUT;
     let mut pieces: Board = PIECE_LAYOUT;
 
+    let mut selected_piece_data: (usize, usize, PieceType, Color) = (0, 0, PieceType::Empty, Color::White);
+    let mut is_selecting_piece: bool = true;
+
     loop {
         clear_background(GRAY);
-        if is_mouse_button_pressed(MouseButton::Left) {
-            move_piece(&mut board, &mut pieces, (1, 2));
+
+        // select or move piece
+        if is_mouse_button_pressed(MouseButton::Left){
+            if is_selecting_piece {selected_piece_data = select_piece(&mut board, &mut pieces);
+                                         is_selecting_piece = !is_selecting_piece}
+            else if !is_selecting_piece {move_piece(&mut board, &mut pieces, selected_piece_data); 
+                                         is_selecting_piece = !is_selecting_piece}
         }
         draw_board(&mut board, &mut pieces);
         next_frame().await;
