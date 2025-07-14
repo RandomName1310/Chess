377714a4658a91fcf3d24c65340861e6b240f677
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2025-07-14 18:31:05 -0300

    Working state system, updated board representation

--- src/board.rs
+++ src/board.rs
@@ -3,8 +3,6 @@ use crate::moves::*;
 pub const BOARD_SIZE: usize = 8;
 pub const SQUARE_SIZE: f32 = 80.0;
 
-pub type Board = [[char; 8]; 8];
-
 #[derive(Copy, Clone, PartialEq)]
 pub enum PieceType {
     Pawn(Moveset),
@@ -30,35 +28,330 @@ pub struct Piece {
     pub color: PieceColor, 
 }
 
-pub const BOARD_LAYOUT: [[char; 8]; 8] = [
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 0 (top)
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 1
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 2
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 3
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 4
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 5
-    ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'], // row 6
-    ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'], // row 7 (bottom)
+#[derive(Copy, Clone, Debug)]
+pub struct Square {
+    pub color: char, 
+    pub piece: char, 
+    pub state: char, 
+}
+
+pub type Board = [[Square; 8]; 8];
+
+pub const BOARD_LAYOUT: Board = [
+    // 0
+    [
+        Square { color: 'B', piece: 'R', state: '_' },
+        Square { color: 'W', piece: 'N', state: '_' },
+        Square { color: 'B', piece: 'B', state: '_' },
+        Square { color: 'W', piece: 'Q', state: '_' },
+        Square { color: 'B', piece: 'K', state: '_' },
+        Square { color: 'W', piece: 'B', state: '_' },
+        Square { color: 'B', piece: 'N', state: '_' },
+        Square { color: 'W', piece: 'R', state: '_' },
+    ],
+    // 1
+    [
+        Square { color: 'W', piece: 'P', state: '_' },
+        Square { color: 'B', piece: 'P', state: '_' },
+        Square { color: 'W', piece: 'P', state: '_' },
+        Square { color: 'B', piece: 'P', state: '_' },
+        Square { color: 'W', piece: 'P', state: '_' },
+        Square { color: 'B', piece: 'P', state: '_' },
+        Square { color: 'W', piece: 'P', state: '_' },
+        Square { color: 'B', piece: 'P', state: '_' },
+    ],
+    //2
+    [
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+    ],
+    // 3
+    [
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+    ],
+    // 4
+    [
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+    ],
+    // 5
+    [
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+        Square { color: 'W', piece: ' ', state: '_' },
+        Square { color: 'B', piece: ' ', state: '_' },
+    ],
+    // 6
+    [
+        Square { color: 'B', piece: 'p', state: '_' },
+        Square { color: 'W', piece: 'p', state: '_' },
+        Square { color: 'B', piece: 'p', state: '_' },
+        Square { color: 'W', piece: 'p', state: '_' },
+        Square { color: 'B', piece: 'p', state: '_' },
+        Square { color: 'W', piece: 'p', state: '_' },
+        Square { color: 'B', piece: 'p', state: '_' },
+        Square { color: 'W', piece: 'p', state: '_' },
+    ],
+    // 7
+    [
+        Square { color: 'W', piece: 'r', state: '_' },
+        Square { color: 'B', piece: 'n', state: '_' },
+        Square { color: 'W', piece: 'b', state: '_' },
+        Square { color: 'B', piece: 'q', state: '_' },
+        Square { color: 'W', piece: 'k', state: '_' },
+        Square { color: 'B', piece: 'b', state: '_' },
+        Square { color: 'W', piece: 'n', state: '_' },
+        Square { color: 'B', piece: 'r', state: '_' },
+    ],
 ];
 
-pub const PIECE_LAYOUT: [[char; 8]; 8] = [
-    // black pieces
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+/* private functions */
 
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+fn get_piece_type(piece_char: char, color: PieceColor, y: usize) -> PieceType{
+    let piece_type = match piece_char.to_ascii_lowercase() {
+        'p' => {
+            if color == PieceColor::Black {
+                if y == 1 { PieceType::Pawn(PAWN_BLACK_DOUBLE_FORWARD) }
+                else      { PieceType::Pawn(PAWN_BLACK_MOVES) }
+            } else {
+                if y == 6 { PieceType::Pawn(PAWN_WHITE_DOUBLE_FORWARD) }
+                else      { PieceType::Pawn(PAWN_WHITE_MOVES) }
+            }
+        }
+        'r' => PieceType::Rook(ROOK_MOVES),
+        'n' => PieceType::Knight(KNIGHT_MOVES),
+        'b' => PieceType::Bishop(BISHOP_MOVES),
+        'q' => PieceType::Queen(QUEEN_MOVES),
+        'k' => PieceType::King(KING_MOVES),
+        _ => PieceType::Empty,
+    };
 
-    // white pieces
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
-];
+    piece_type
+}
+
+// calculate state of all Moveset offsets
+fn calculate_offsets(board: &mut Board, 
+                     path: Moveset, 
+                     color: PieceColor, 
+                     white_attacks: &mut [[bool; 8]; 8], 
+                     black_attacks: &mut [[bool; 8]; 8], 
+                     (x, y): (usize, usize), 
+                     color_this_path: bool){
+    // Go through each move offset
+    for offset in path {
+        let mut pos_x = x as isize;
+        let mut pos_y = y as isize;
+
+        for _ in 0..offset.repeats {
+            pos_x += offset.dx as isize;
+            pos_y += offset.dy as isize;
+
+            if pos_x < 0 || pos_x >= BOARD_SIZE as isize || pos_y < 0 || pos_y >= BOARD_SIZE as isize {
+                break;
+            }
+
+            let tx = pos_x as usize;
+            let ty = pos_y as usize;
+
+
+            let target_piece = board[ty][tx].piece;
+
+            let is_enemy = match color {
+                PieceColor::White => target_piece.is_uppercase(),
+                PieceColor::Black => target_piece.is_lowercase(),
+            };
+
+            if color_this_path && (target_piece == ' ' || is_enemy) {
+                color_square(board, (tx, ty), 'R');
+            }
+
+            if target_piece != ' ' {
+                if (color == PieceColor::White && target_piece.is_uppercase()) ||
+                    (color == PieceColor::Black && target_piece.is_lowercase()) {
+                    match color {
+                        PieceColor::White => white_attacks[ty][tx] = true,
+                        PieceColor::Black => black_attacks[ty][tx] = true,
+                    }
+                }
+                break; // can't jump over pieces
+            }
+
+            // empty tile is always reachable
+            match color {
+                PieceColor::White => white_attacks[ty][tx] = true,
+                PieceColor::Black => black_attacks[ty][tx] = true,
+            }
+        }
+    }
+}
+
+
+// calculate state of special pawn capture
+fn calculate_pawn_capture(piece: Piece, board: &mut Board,
+                     color: PieceColor, 
+                     white_attacks: &mut [[bool; 8]; 8], 
+                     black_attacks: &mut [[bool; 8]; 8], 
+                     (x, y): (usize, usize)){
+    let captures = match color {
+        PieceColor::White => PAWN_WHITE_CAPTURES,
+        PieceColor::Black => PAWN_BLACK_CAPTURES,
+    };
+
+    for offset in captures {
+        let tx = x as isize + offset.dx as isize;
+        let ty = y as isize + offset.dy as isize;
+
+        // outside the board?
+        if tx < 0 || tx >= BOARD_SIZE as isize || ty < 0 || ty >= BOARD_SIZE as isize {
+            continue;
+        }
+
+        let (tx, ty) = (tx as usize, ty as usize);
+        let target_piece = board[ty][tx].piece;
+
+        // skip empty squares
+        if target_piece == ' ' {
+            continue;
+        }
+
+        // mark only if the piece is an opponent’s
+        let is_enemy = match color {
+            PieceColor::White => target_piece.is_uppercase(), 
+            PieceColor::Black => target_piece.is_lowercase(),
+        };
+
+        if is_enemy && piece.x == x && piece.y == y{
+            color_square(board, (tx as usize, ty as usize), 'R');
+            match color {
+                PieceColor::White => white_attacks[ty][tx] = true,
+                PieceColor::Black => black_attacks[ty][tx] = true,
+            }
+        }
+    }
+}
+
+/* public functions */
+
+// color specific square
+pub fn color_square(board: &mut Board, coord: (usize, usize), color: char){
+    board[coord.1][coord.0].color = color;
+}
+
+// sets the "state" parameter on main board and color square of pieces-> which piece attack where
+pub fn set_board_state(board: &mut Board, piece: Piece){
+    for row in board.iter_mut() {
+        for square in row.iter_mut() {
+            square.state = '_';
+        }
+    }
+
+    // Temporary buffer to track which side attacks which square
+    let mut white_attacks = [[false; 8]; 8];
+    let mut black_attacks = [[false; 8]; 8];
+
+    color_square(board, (piece.x, piece.y), 'G');
+
+    for y in 0..BOARD_SIZE {
+        for x in 0..BOARD_SIZE {
+            // get char in the specific square
+            let piece_char = board[y][x].piece;
+
+            if piece_char == ' ' {
+                continue; 
+            }
+
+            // get color
+            let color = if piece_char.is_uppercase() {
+                PieceColor::Black
+            } else {
+                PieceColor::White
+            };
+
+            // get the PieceType to unwrap after
+            let piece_type = get_piece_type(piece_char, color, y);
+
+            // unwraps PieceType
+            let path = match piece_type {
+                PieceType::Pawn(m) |
+                PieceType::Rook(m) |
+                PieceType::Knight(m) |
+                PieceType::Bishop(m) |
+                PieceType::Queen(m) |
+                PieceType::King(m) => m,
+                PieceType::Empty => continue,
+            };
+
+            // check if the path of a piece can be colored
+            let mut color_this_path = false;
+
+            if (x, y) == (piece.x, piece.y){
+                color_this_path = true;
+            }
+
+            // calculates the offsets of the pieces and colors the squares
+            calculate_offsets(board,
+                path,
+                color,
+                &mut white_attacks,
+                &mut black_attacks,
+                (x, y),
+                color_this_path,);
+
+            // calculate special pawn diagonal captures
+            if let PieceType::Pawn(_) = piece_type {
+                calculate_pawn_capture(piece,
+                board,
+                color,
+                &mut white_attacks,
+                &mut black_attacks,
+                (x, y));
+            }
+        }
+    }
+
+    // apply the final state per square
+    for y in 0..BOARD_SIZE {
+        for x in 0..BOARD_SIZE {
+            board[y][x].state = match (white_attacks[y][x], black_attacks[y][x]) {
+                (true, true)   => 'T',
+                (true, false)  => 'W',
+                (false, true)  => 'B',
+                (false, false) => '_',
+            };
+        }
+    }
+}
 
 
 
-// makes all squares return to their original coll
-pub fn refresh_board(board: &mut Board){
-    *board = BOARD_LAYOUT;
+// makes all squares return to their original color
+pub fn refresh_board_color(board: &mut Board){
+    for (row_idx, row) in board.iter_mut().enumerate() {
+        for (col_idx, square) in row.iter_mut().enumerate() {
+            square.color = BOARD_LAYOUT[row_idx][col_idx].color;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -15,78 +15,7 @@ fn get_board_offset() -> (f32, f32){
     (x_offset, y_offset)
 }
 
-fn color_pieces_path(piece: Piece, board: &mut Board, pieces: &mut Board){
-    // unwrap moveset
-    let path: Moveset = match piece.piece_type {
-        PieceType::Pawn(m)   => m,
-        PieceType::Rook(m)   => m,
-        PieceType::Knight(m) => m,
-        PieceType::Bishop(m) => m,
-        PieceType::Queen(m)  => m,
-        PieceType::King(m)   => m,
-        PieceType::Empty     => {
-            println!("Empty piece has no moveset.");
-            return;
-        }
-    };
-
-    // color square clicked
-    board[piece.y][piece.x] = 'G';
-
-    // normal path (forward moves)
-    for offset in path {
-        let mut pos_x = piece.x as isize;
-        let mut pos_y = piece.y as isize;
-
-        for _ in 0..offset.repeats {
-            pos_x += offset.dx as isize;
-            pos_y += offset.dy as isize;
-
-            if pos_x < 0 || pos_x >= BOARD_SIZE as isize || pos_y < 0 || pos_y >= BOARD_SIZE as isize {
-                break;
-            }
-
-            let target = pieces[pos_y as usize][pos_x as usize];
-
-            if target != ' ' {
-                if piece.color == PieceColor::Black && target.is_lowercase()
-                || piece.color == PieceColor::White && target.is_uppercase() {
-                    board[pos_y as usize][pos_x as usize] = 'R';
-                }
-                break;
-            }
-
-            board[pos_y as usize][pos_x as usize] = 'R';
-        }
-    }
-
-    // capture path (only for pawns)
-    if let PieceType::Pawn(_) = piece.piece_type {
-        let captures = match piece.color {
-            PieceColor::White => PAWN_WHITE_CAPTURES,
-            PieceColor::Black => PAWN_BLACK_CAPTURES,
-        };
-
-        for offset in captures {
-            let pos_x = piece.x as isize + offset.dx as isize;
-            let pos_y = piece.y as isize + offset.dy as isize;
-
-            if pos_x < 0 || pos_x >= BOARD_SIZE as isize || pos_y < 0 || pos_y >= BOARD_SIZE as isize {
-                continue;
-            }
-
-            let target = pieces[pos_y as usize][pos_x as usize];
-
-            if (piece.color == PieceColor::Black  && target.is_lowercase()) ||
-               (piece.color == PieceColor::White  && target.is_uppercase())
-            {
-                board[pos_y as usize][pos_x as usize] = 'R';
-            }
-        }
-    }
-}
-
-
+// get the coords of a selected square
 fn get_selected_square() -> (usize, usize){
     let mouse_pos: (f32, f32) = mouse_position();
     let board_offset: (f32, f32) = get_board_offset();
@@ -99,30 +28,30 @@ fn get_selected_square() -> (usize, usize){
 }
 
 // draw the board with the pieces on screen
-fn draw_board(board: &mut Board, pieces: &mut Board){
+fn draw_board(board: &mut Board){
     let board_offset: (f32, f32) = get_board_offset();
 
     // set pieces and squares
     for x in 0..BOARD_SIZE {
         for y in 0..BOARD_SIZE {
-            let piece: char = pieces[y][x];
+            let piece: char = board[y][x].piece;
 
             let pos_x = board_offset.0 + x as f32 * SQUARE_SIZE;
             let pos_y = board_offset.1 + y as f32 * SQUARE_SIZE;
 
-            if board[y][x] == 'W'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
-            else if board[y][x] == 'B'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)}
-            else if board[y][x] == 'R'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, RED)} 
-            else if board[y][x] == 'G'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, GREEN)} 
+            if board[y][x].color == 'W'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, WHITE)} 
+            else if board[y][x].color == 'B'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, BLACK)}
+            else if board[y][x].color == 'R'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, RED)} 
+            else if board[y][x].color == 'G'{draw_rectangle(pos_x, pos_y, SQUARE_SIZE, SQUARE_SIZE, GREEN)} 
             draw_text(&format!("{}", piece), pos_x + 30.0, pos_y + 60.0, 50.0, GRAY);
         }
     }
 }
 
 // select a piece from the board
-fn select_piece(board: &mut Board, pieces: &mut Board) -> Piece {
+fn select_piece(board: &mut Board) -> Piece {
     let (square_x, square_y) = get_selected_square();
-    let piece_ch: char = pieces[square_y][square_x];
+    let piece_ch: char = board[square_y][square_x].piece;
 
     let color = if piece_ch.is_uppercase() {PieceColor::Black } else {PieceColor::White };
 
@@ -149,7 +78,7 @@ fn select_piece(board: &mut Board, pieces: &mut Board) -> Piece {
     };
 
     // remove as casas vermelhas da seleção anterior
-    refresh_board(board);
+    refresh_board_color(board);
 
     let piece = Piece {
         x: square_x,
@@ -159,20 +88,19 @@ fn select_piece(board: &mut Board, pieces: &mut Board) -> Piece {
     };
 
     // colore o caminho possível da peça recém‑selecionada
-    color_pieces_path(piece, board, pieces);
+    set_board_state(board, piece);
 
     piece
 }
 
 // move a piece from the board
-fn move_piece(board: &mut Board, pieces: &mut Board, last_piece_data: Piece){
+fn move_piece(board: &mut Board, last_piece_data: Piece){
     let (square_x,square_y): (usize, usize) = get_selected_square();
     let Piece{x: piece_x, y: piece_y, piece_type, color}: Piece = last_piece_data;
 
-   
-    if board[square_y][square_x] == 'R'{
-        pieces[piece_y][piece_x] = ' ';
-        pieces[square_y][square_x] = match piece_type{
+    if board[square_y][square_x].color == 'R'{
+        board[piece_y][piece_x].piece = ' ';
+        board[square_y][square_x].piece = match piece_type{
             PieceType::Pawn(_) => if color == PieceColor::Black{'P'}else{'p'},
             PieceType::Bishop(_) => if color == PieceColor::Black{'B'}else{'b'},
             PieceType::Knight(_) => if color == PieceColor::Black{'N'}else{'n'},
@@ -184,13 +112,12 @@ fn move_piece(board: &mut Board, pieces: &mut Board, last_piece_data: Piece){
     }
 
     // remove red squares from previous calls
-    refresh_board(board);
+    refresh_board_color(board);
 }
 
 #[macroquad::main("CHESS")]
 async fn main(){
     let mut board: Board = BOARD_LAYOUT;
-    let mut pieces: Board = PIECE_LAYOUT;
 
     let mut selected_piece: Piece = {Piece{x: 0, y: 0, piece_type: PieceType::Empty, color: PieceColor::White}};
 
@@ -201,10 +128,10 @@ async fn main(){
         if is_mouse_button_pressed(MouseButton::Left){
             let (square_x, square_y) = get_selected_square();
 
-            if board[square_y][square_x] == 'R' {move_piece(&mut board, &mut pieces, selected_piece);}
-            else{selected_piece = select_piece(&mut board, &mut pieces);}
+            if board[square_y][square_x].color == 'R' {move_piece(&mut board, selected_piece);}
+            else{selected_piece = select_piece(&mut board);}
         }
-        draw_board(&mut board, &mut pieces);
+        draw_board(&mut board);
         next_frame().await;
     }
 }
